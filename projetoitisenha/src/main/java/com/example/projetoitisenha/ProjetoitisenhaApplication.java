6fef06f00209645a89ea858d626aed4c4ba64335
Rafmadragon <rafael.m_vieira@hotmail.com>
2021-04-06 19:40:18 -0300

    criação de projeto para desafio iti

---
+++ projetoitisenha/src/main/java/com/example/projetoitisenha/ProjetoitisenhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoitisenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoitisenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoitisenhaApplication.class, args);
+	}
+
+}
---
+++ projetoitisenha/src/test/java/com/example/projetoitisenha/ProjetoitisenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoitisenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoitisenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
